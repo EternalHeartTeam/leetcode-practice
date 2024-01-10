0806ab9b29a2f2311314c0dbe84b8c72029046b3
wh131462 <hao131462@qq.com>
2024-01-10 15:35:23 +0800

    feat:2696 & 创建脚本和检测脚本

---
+++ common/create.js
@@ -0,0 +1,19 @@
+/**
+ * 工作区为 根目录
+ */
+const fs = require("fs");
+const {temExe} = require("./utils/temExe");
+
+const args = process.argv.slice(2);
+const path = args[0];
+// 判断是否存在
+fs.exists(`./src/${path}`, (exists) => {
+    if (exists) {
+        console.log(`./src/${path} 已存在,请检查目录!`);
+        return;
+    }
+    temExe('mkdir -p ./src/{0} && cp ./common/template/template.js ./src/{0}/index.js')
+        .then(() => console.log(`创建成功`))
+        .catch(e => console.log("创建报错: ", e));
+})
+
---
+++ common/template/template.js
@@ -0,0 +1,13 @@
+/**
+ * @题目
+ *
+ * @描述:
+ *
+ */
+const test = () => {
+
+};
+/**
+ * Test case
+ */
+console.log(test());
