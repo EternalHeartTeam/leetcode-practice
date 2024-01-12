d613d0399e760d66e71e03cb6adc257d85d19812
wh131462 <hao131462@qq.com>
2024-01-12 22:46:31 +0800

    fix:修复随意删除的问题

--- common/utils/createQuestion.js
+++ common/utils/createQuestion.js
@@ -3,28 +3,28 @@ const path = require("path");
 
 function createQuestion(newPath){
     return new Promise((resolve, reject) => {
+        const newDir = path.normalize( `./src/${newPath}`);
+        const sourceFilePath = path.normalize("./common/template/template.js")
+        const newFilePath = path.join(newDir,"index.js")
         // 判断是否存在
-        fs.exists(newPath, (exists) => {
+        fs.exists(newFilePath, (exists) => {
             if (exists) {
                 reject(`./src/${newPath} 已存在,请检查目录!`);
-                return;
-            }
-            try {
-                const newDir = path.normalize( `./src/${newPath}`);
-                const sourceFilePath = path.normalize("./common/template/template.js")
-                const newFilePath = path.join(newDir,"index.js")
-                // 创建目录
-                fs.mkdir(newDir, { recursive: true },()=>{
-                    // 复制文件
-                    fs.copyFile(sourceFilePath, newFilePath,0,()=>{
-                        resolve(newFilePath);
-                        console.log(`File '${sourceFilePath}' copied to '${newFilePath}'`);
-                    });
-                });
-            } catch (error) {
-                reject('Error:', error.message);
-            }
+            }else{
+                try {
 
+                    // 创建目录
+                    fs.mkdir(newDir, { recursive: true },()=>{
+                        // 复制文件
+                        fs.copyFile(sourceFilePath, newFilePath,0,()=>{
+                            resolve(newFilePath);
+                            console.log(`File '${sourceFilePath}' copied to '${newFilePath}'`);
+                        });
+                    });
+                } catch (error) {
+                    reject('Error:', error.message);
+                }
+            }
         })
     })
 }
