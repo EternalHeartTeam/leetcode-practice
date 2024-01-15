bd1d01b4a440f8f80dfc67b04180dde29bac9da9
SmallTeddy <zhangzeming1212@gmail.com>
2024-01-15 11:58:45 +0800

    fix: log

--- common/utils/withTimeLog.js
+++ common/utils/withTimeLog.js
@@ -3,22 +3,24 @@ const { getFileSize } = require("./sizeUtil");
 const { isSameData } = require("./isSameData");
 
 function withTimeLog(callback, compare) {
-    console.time("函数执行用时");
+    // 记录开始时间
+    const startTime = performance.now();
     // 获取函数执行前的内存使用情况
     const startHeapStatsArray = v8.getHeapSpaceStatistics()
     const callVal = callback()
     // 获取函数执行后的内存使用情况
     const endHeapStatsArray = v8.getHeapSpaceStatistics()
-    console.log(callVal);
-    console.timeEnd("函数执行用时")
+    // 记录结束时间
+    const endTime = performance.now();
     const startHeapStats = startHeapStatsArray.reduce((prev, curr) => prev += curr.space_used_size, 0)
     const endHeapStats = endHeapStatsArray.reduce((prev, curr) => prev += curr.space_used_size, 0)
-    // 计算内存使用情况（以字节为单位）
-    const heapMemoryUsageInBytes = endHeapStats - startHeapStats
-    // 计算内存使用情况
-    console.log(`内存占用：${getFileSize(heapMemoryUsageInBytes)}`)
 
-    console.log(`预期值:${JSON.stringify(compare)},${isSameData(callVal, compare) ? "测试通过!" : "测试未通过!"}`)
+    console.table({
+        '函数执行结果': JSON.stringify(callVal),
+        '函数执行用时': Number(endTime - startTime).toFixed(4) + 'ms',
+        '内存占用': getFileSize(endHeapStats - startHeapStats),
+        [`预期值${JSON.stringify(compare)}`]: isSameData(callVal, compare) ? "测试通过!" : "测试未通过!"
+    })
     console.log('--------------------------')
 }
 
