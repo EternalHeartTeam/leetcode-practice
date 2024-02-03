b12bce29e1c3d2aad397376a4fef59a42feff153
Hedwig-Fang <706964007@qq.com>
2024-02-04 00:04:48 +0800

    chore: eslint fix

--- common/utils/sizeUtil.js
+++ common/utils/sizeUtil.js
@@ -1,22 +1,13 @@
-//千
+// 千
 const KB = 1024;
-//兆
+// 兆
 const MB = 1024 * KB;
-//吉
+// 吉
 const GB = 1024 * MB;
-//太
+// 太
 const TB = 1024 * GB;
-//拍
+// 拍
 const PB = 1024 * GB;
-/**
- * 获取文件的尺寸
- * @param size 文件大小
- * @param precision 小数位
- */
-function getFileSize(size, precision = 2) {
-    const fileSizeType = getFileSizeUnit(size);
-    return `${(size / fileSizeType.size).toFixed(precision)} ${fileSizeType.label}`;
-}
 
 /**
  * 获取文件的单位
@@ -24,19 +15,27 @@ function getFileSize(size, precision = 2) {
  * @return {size: number, label: string}
  */
 function getFileSizeUnit(size) {
-    if (size < KB) {
-        return { size: 1, label: 'B' };
-    } else if (size < MB) {
-        return { size: KB, label: 'KB' };
-    } else if (size < GB) {
-        return { size: MB, label: 'MB' };
-    } else if (size < TB) {
-        return { size: GB, label: 'GB' };
-    } else if (size < PB) {
-        return { size: TB, label: 'TB' };
-    } else {
-        return { size: PB, label: 'PB' };
-    }
+  if (size < KB) {
+    return { size: 1, label: 'B' };
+  } if (size < MB) {
+    return { size: KB, label: 'KB' };
+  } if (size < GB) {
+    return { size: MB, label: 'MB' };
+  } if (size < TB) {
+    return { size: GB, label: 'GB' };
+  } if (size < PB) {
+    return { size: TB, label: 'TB' };
+  }
+  return { size: PB, label: 'PB' };
+}
+/**
+ * 获取文件的尺寸
+ * @param size 文件大小
+ * @param precision 小数位
+ */
+function getFileSize(size, precision = 2) {
+  const fileSizeType = getFileSizeUnit(size);
+  return `${(size / fileSizeType.size).toFixed(precision)} ${fileSizeType.label}`;
 }
 
-module.exports = { getFileSize, getFileSizeUnit }
+module.exports = { getFileSize, getFileSizeUnit };
