f880cc9a03c818e7a6826aafac3f7de0e24aa4de
Hedwig-Fang <706964007@qq.com>
2024-02-04 00:04:32 +0800

    chore: eslint fix

--- common/scripts/check.js
+++ common/scripts/check.js
@@ -6,13 +6,15 @@ const {readStore} = require("../utils/store");
 const args = process.argv.slice(2);
 let name;
 switch (args[0]) {
-    case "-r":
+    case "-r":  {
         const random = readStore("random-question-info")
         name = `${random.id}.${random.enName}`;
         console.log(`[leet-check]检测当前随机题目:${name}`)
         break;
-    case "-i":
-        id = args[1];
+    }
+
+    case "-i": {
+       let id = args[1];
         const specified = readStore("specified-question-info")
         name = `${specified.id}.${specified.enName}`;
         if(id===undefined&&name.startsWith("undefined")) {
@@ -21,11 +23,14 @@ switch (args[0]) {
         }
         console.log(`[leet-check]检测题目:${name}`)
         break;
+    }
+      
     case "-t":
-    default:
+    default: {
         const question = readStore("today-question-info")
         name = `${question.id}.${question.enName}`;
         console.log(`[leet-check]检测题目:${name}`)
+    }
         break;
 }
 temExe('node ./src/{0}/index.js',name)
