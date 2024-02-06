95be0be7f8963155d64535efd16341bc7565db2b
EternalHeart <hao131462@qq.com>
2024-02-06 18:20:26 +0800

    fix:指令重构

--- bin/lc.js
+++ bin/lc.js
@@ -1,48 +1,43 @@
 #! /usr/bin/env node
 import {program} from "commander";
+import {artFontLogo} from "../resources/text/art-font-logo.js";
+import {examples} from "../resources/text/examples.js";
+import {love} from "../resources/text/love.js";
+import {aim} from "../resources/text/aim.js";
+import {referMode} from "#common/utils/create-check/refer-mode.js";
+import {getArgs} from "#common/utils/create-check/get-args.js";
+import fs from "fs";
+const {version} =  JSON.parse(fs.readFileSync("package.json",'utf-8'));
 
 program
-    .version('1.0.0')
-    .description(`                                                                                                                                                                                  
-
-░▒▓█▓▒░      ░▒▓████████▓▒░▒▓████████▓▒░▒▓████████▓▒░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓███████▓▒░░▒▓████████▓▒░      ░▒▓███████▓▒░░▒▓███████▓▒░ ░▒▓██████▓▒░ ░▒▓██████▓▒░▒▓████████▓▒░▒▓█▓▒░░▒▓██████▓▒░░▒▓████████▓▒░ 
-░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░         ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░             ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░   ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
-░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░         ░▒▓█▓▒░  ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░             ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        ░▒▓█▓▒░   ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░        
-░▒▓█▓▒░      ░▒▓██████▓▒░ ░▒▓██████▓▒░    ░▒▓█▓▒░  ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓██████▓▒░        ░▒▓███████▓▒░░▒▓███████▓▒░░▒▓████████▓▒░▒▓█▓▒░        ░▒▓█▓▒░   ░▒▓█▓▒░▒▓█▓▒░      ░▒▓██████▓▒░   
-░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░         ░▒▓█▓▒░  ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░             ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        ░▒▓█▓▒░   ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░        
-░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░         ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░             ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░   ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
-░▒▓████████▓▒░▒▓████████▓▒░▒▓████████▓▒░  ░▒▓█▓▒░   ░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓███████▓▒░░▒▓████████▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓██████▓▒░░▒▓████████▓▒░ 
-                                                                                                                                                                                                                
-                                                                                                                                                                                                                
-LeetCode Practice CLI`)
-    .argument('<string>', 'Default to get this identity.')
+    .version(version)
+    .description(`${artFontLogo}\n${aim}`)
+    .addHelpText('after', examples+love)
+    .arguments("[identity]")
     .option('-i, --identity <identity>', 'Specify a question by identity.')
     .option('-r, --random', 'Get a question randomly.')
     .option('-t, --today', 'Get a question today.')
-    .parse(process.argv);
-// program.
-//     examples(`
-//   $ lc
-//   $ lc 1314
-//   $ lc -i 1314
-//   $ lc -r
-//   $ lc -t
-// `)
-const args = program.args;
-const opts = program.opts();
-console.log("[args]",args)
-console.log("[opts]",opts)
+    .parse(process.argv)
 
-const getCase = (args,opts)=>{
-    return 'today'
+const cmdArgs = program.args;
+const cmdOpts = program.opts();
+// 模式对应的action
+const callModeAction = {
+    'today': () => {
+        // todo 获取今日题目的进程
+        console.log("[leetcode-practice] 获取今日题目")
+    },
+    'random': () => {
+        // todo 获取随机题目的进程
+        console.log("[leetcode-practice] 获取随机题目")
+    },
+    'identity': (id) => {
+        // todo 获取指定题目的进程
+        console.log(`[leetcode-practice] 获取指定题目[${id}]`)
+    },
 }
-
-const rules = {
-    'today':(args,opts)=>{},
-    'random':(args,opts)=>{},
-    'identity':(args,opts)=>{},
-}
-
+// 获取模式和参数
+const mode = referMode(cmdArgs, cmdOpts);
+const args = getArgs(mode,cmdArgs,cmdOpts);
 // 执行指令分发
-const current = getCase(args,opts);
-rules[current](args,opts);
+callModeAction[mode](args);
--- bin/lk.js
+++ bin/lk.js
@@ -1,6 +1,43 @@
 #! /usr/bin/env node
-const init = async() => {
-  await import('../common/scripts/check.js');
- 
- }
- init()
+import {program} from "commander";
+import {artFontLogo} from "../resources/text/art-font-logo.js";
+import {examples} from "../resources/text/examples.js";
+import {love} from "../resources/text/love.js";
+import {aim} from "../resources/text/aim.js";
+import {referMode} from "#common/utils/create-check/refer-mode.js";
+import {getArgs} from "#common/utils/create-check/get-args.js";
+import fs from "fs";
+const {version} =  JSON.parse(fs.readFileSync("package.json",'utf-8'));
+
+program
+    .version(version)
+    .description(`${artFontLogo}\n${aim}`)
+    .addHelpText('after', examples+love)
+    .arguments("[identity]")
+    .option('-i, --identity <identity>', 'Check the specified question by identity.')
+    .option('-r, --random', 'Check the last random question.')
+    .option('-t, --today', 'Check the question today.')
+    .parse(process.argv)
+
+const cmdArgs = program.args;
+const cmdOpts = program.opts();
+// 模式对应的action
+const callModeAction = {
+    'today': () => {
+        // todo 获取今日题目的进程
+        console.log("[leetcode-practice] 检测今日题目")
+    },
+    'random': () => {
+        // todo 获取随机题目的进程
+        console.log("[leetcode-practice] 检测随机题目")
+    },
+    'identity': (id) => {
+        // todo 获取指定题目的进程
+        console.log(`[leetcode-practice] 检测指定题目[${id}]`)
+    },
+}
+// 获取模式和参数
+const mode = referMode(cmdArgs, cmdOpts);
+const args = getArgs(mode,cmdArgs,cmdOpts);
+// 执行指令分发
+callModeAction[mode](args);
