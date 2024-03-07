f80c59e3bca7b9ccfeb74fbb18d1676fa56d8659
EternalHeart <hao131462@qq.com>
2024-03-07 21:17:58 +0800

    fix:fix the search json import in getQuestionKeyword

--- common/utils/question-getter/getQuestionByKeyword.js
+++ common/utils/question-getter/getQuestionByKeyword.js
@@ -1,7 +1,9 @@
-import { getQuestionKeywordJson } from '#resources/headers/questionKeywordJson.js'
 import { graphql } from '#common/utils/http/graphql.js'
+import {getQuestionSearchJson} from "#resources/headers/questionSearchJson.js";
 
 export async function getQuestionByKeyword(keyword) {
-  const questionData = await graphql(getQuestionKeywordJson(keyword.toString()))
+  const questionData = await graphql(getQuestionSearchJson(keyword.toString()))
+  console.log(questionData)
   return questionData?.data?.problemsetQuestionList?.questions
 }
+
