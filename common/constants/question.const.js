ed1aa192561121e6ef68f151250882751c6a1c12
EternalHeart <hao131462@qq.com>
2024-03-08 23:24:41 +0800

    fix:get random question actually

--- common/constants/question.const.js
+++ common/constants/question.const.js
@@ -1,13 +1,13 @@
+// region 项目相关
 // 默认语言
 export const DefaultLang = 'javascript'
-
 // 默认版本号
 export const DefaultVer = '0.0.0'
-
 // 包名
 export const PackageName = 'leetcode-practice'
 // github 主账号
-export const GITHUB_HOST = 'wh131462'
+export const GITHUB_HOST = 'EternalHeartTeam'
+// endregion
 // region 域名列表
 // npm 主域名
 export const NPM_URL = 'https://registry.npmjs.org/'
@@ -17,4 +17,8 @@ export const GITHUB_RAW = 'https://raw.githubusercontent.com/'
 export const GITHUB_URL = 'https://github.com/'
 // gitee
 export const GITEE_URL = 'https://gitee.com/'
+// endregion
+// region 实用变量
+// 默认请求数据数量限制
+export const DefaultLimit = 50
 // endregion
