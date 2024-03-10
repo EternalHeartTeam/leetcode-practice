7254f2988a656c28c0e7fbc685e16ff85cc1cbe6
EternalHeart <hao131462@qq.com>
2024-03-10 13:17:48 +0800

    fix(store): fix the bug for create dir outside

--- common/utils/file/getRootPath.js
+++ common/utils/file/getRootPath.js
@@ -1,9 +1,8 @@
 import path from 'node:path'
 import { __dirname } from '#common/utils/file/getDirname.js'
 import { currentEnv } from '#common/utils/etc/checkEnv.js'
-import { PackageName } from '#common/constants/question.const.js'
-
+// 在cli环境下 执行目录为 bin 目录 根目录就是上一层目录
 export const rootPath =
   currentEnv() === 'project'
     ? path.dirname(path.dirname(path.dirname(__dirname)))
-    : path.join(path.dirname(__dirname), PackageName)
+    : path.dirname(__dirname)
