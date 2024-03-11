1289fda262d0821c185f4a69ae76a8dc5921ed66
thinkasany <480968828@qq.com>
2024-03-11 10:31:09 +0800

    fix: update

--- common/utils/file/getRootPath.js
+++ common/utils/file/getRootPath.js
@@ -1,6 +1,7 @@
 import path from 'node:path'
 import { __dirname } from '#common/utils/file/getDirname.js'
 import { currentEnv } from '#common/utils/etc/checkEnv.js'
+
 // 在cli环境下 执行目录为 bin 目录 根目录就是上一层目录
 export const rootPath =
   currentEnv() === 'project'
