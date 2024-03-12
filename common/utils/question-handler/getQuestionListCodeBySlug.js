44851825eb92b2f1da98f1bb78c71c3f0fe27856
EternalHeart <hao131462@qq.com>
2024-03-12 21:43:23 +0800

    feat: add fetch qurtion list

---
+++ common/utils/question-handler/getQuestionListCodeBySlug.js
@@ -0,0 +1,25 @@
+import { createQuestionByTitleSlug } from '../create-check/createUtil.js'
+import { getPlanQuestionList } from '#common/utils/question-getter/getPlanQuestionList.js'
+import { logger } from '#common/utils/logger/logger.js'
+
+// 获取创建promise列表
+async function createCreatePromiseList(slugList, baseDir = process.cwd()) {
+  return slugList.map((titleSlug) => {
+    return createQuestionByTitleSlug(titleSlug, baseDir)
+  })
+}
+
+/**
+ * 創建題目列表通過plan slug
+ * @param slug
+ * @param baseDir
+ */
+export async function getQuestionListCodeBySlug(slug, baseDir) {
+  const { planSubGroups } = await getPlanQuestionList(slug)
+  const questionTitleList = planSubGroups.reduce((acc, cur) => {
+    acc.push(...cur.questions.map((res) => res.titleSlug))
+    return acc
+  }, [])
+  const promiseList = await createCreatePromiseList(questionTitleList, baseDir)
+  return await Promise.allSettled(promiseList)
+}
