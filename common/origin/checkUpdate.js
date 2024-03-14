6b710a1a2ff088d9806d33153bf811e5a5e19306
EternalHeart <48346853+wh131462@users.noreply.github.com>
2024-03-14 22:35:12 +0800

    fix: fix check info log (#52)

--- common/origin/checkUpdate.js
+++ common/origin/checkUpdate.js
@@ -12,14 +12,14 @@ import { PackageName } from '#common/constants/question.const.js'
 const { timestamp, hasShow } = (await getStore('checkResult')) ?? {}
 if (Date.now() - timestamp <= Day || hasShow) process.exit(0)
 
-const { localVersion, remoteVersion, isCliUpdate } = await checkUpdate()
+const { localVersion, npmVersion, isCliUpdate } = await checkUpdate()
 const needShow = false
 if (isCliUpdate) {
   const installInfo = [NpmInstall, YarnInstall, PnpmInstall]
     .map((fun) => fun(PackageName, true, true)) // 暂时先默认为全局
     .join('\n')
   logger.warn(
-    `[leetcode-practice] 检测到新版本[ ${remoteVersion} ]已经发布! 您当前的版本为[ ${localVersion} ]! 您可以执行对应的指令进行手动更新~`
+    `[leetcode-practice] 检测到新版本[ ${npmVersion} ]已经发布! 您当前的版本为[ ${localVersion} ]! 您可以执行对应的指令进行手动更新~`
   )
   logger.info(`${installInfo}`)
 }
