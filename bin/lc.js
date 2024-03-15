e3f484a2e85f6721e2a9c178be193dbab5fd0753
SmallTeddy <zhangzeming1212@gmail.com>
2024-03-15 17:22:18 +0800

    fix: allQuestion controller

--- bin/lc.js
+++ bin/lc.js
@@ -62,7 +62,7 @@ export const callModeAction = {
   },
   all: async () => {
     const allQuestionData = await getAllQuestionList()
-    for await(const question of allQuestionData) setAllQuestion(question)
+    for await (const question of allQuestionData) setAllQuestion(question)
     process.exit(0)
   }
 }
