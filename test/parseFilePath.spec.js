2bdd0d8b8a396867d8d6c3b0af00a5abd440bd59
hyfree-msi <wangxianqiangcn@foxmail.com>
2024-03-16 07:59:05 +0800

    feat: get 'jsonExampleTestcases', 'exampleTestcases'

--- test/parseFilePath.spec.js
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from 'vitest'
-import { parseFilePath } from '#common/utils/file/parseFilePath.js'
-
-const path = 'src/LRC 257.pp'
-describe('parseFilePath', () => {
-  it('是否正确的转化了地址', () => {
-    expect(parseFilePath(path)).equal(`\"src/LRC 257.pp\"`, '正确解析地址')
-  })
-})
